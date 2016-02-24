9e7c3d3f99168f00fb2fd2ed084ad6563a2a078f
Antonio Zugaldia <antonio@mapbox.com>
2016-02-24 10:21:21 -0500

    [android] #3891 - make reason a more idiomatic ErrorReason Android IntDef and remove unnecessary constructor

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineRegionError.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineRegionError.java
@@ -1,18 +1,29 @@
 package com.mapbox.mapboxsdk.offline;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * An Offline Region error
  */
 public class OfflineRegionError {
 
     /**
-     * Success = 1
-     * NotFound = 2
-     * Server = 3
-     * Connection = 4
-     * Other = 6
+     * Error code, self-explanatory.
      */
-    private int reason;
+    @IntDef({REASON_SUCCESS, REASON_NOT_FOUND, REASON_SERVER, REASON_CONNECTION, REASON_OTHER})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ErrorReason {}
+
+    public static final int REASON_SUCCESS = 1;
+    public static final int REASON_NOT_FOUND = 2;
+    public static final int REASON_SERVER = 3;
+    public static final int REASON_CONNECTION = 4;
+    public static final int REASON_OTHER = 6;
+
+    private @ErrorReason int reason;
 
     /**
     /* An error message from the request handler, e.g. a server message or a system message
@@ -28,16 +39,11 @@ public class OfflineRegionError {
         // For JNI use only
     }
 
-    public OfflineRegionError(int reason, String message) {
-        this.reason = reason;
-        this.message = message;
-    }
-
     /*
      * Getters
      */
 
-    public int getReason() {
+    public @ErrorReason int getReason() {
         return reason;
     }
 
